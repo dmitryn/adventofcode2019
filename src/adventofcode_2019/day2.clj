9a33af14a4203e04d538c54df4b3967d43278deb
Dima Novotochinov <trybeee@gmail.com>
2019-12-02 22:16:05 +0100

    (wip) day2 part 2

--- src/adventofcode_2019/day2.clj
+++ src/adventofcode_2019/day2.clj
@@ -4,29 +4,40 @@
 (defn- parse-input [s]
   (clojure.string/split s #","))
 
-(defn- prepare-program [xs]
+(defn- prepare-program [xs a b]
   (-> xs
-      (assoc 1 12)
-      (assoc 2 2)))
+      (assoc 1 a)
+      (assoc 2 b)))
 
-(defn run []
-  (let [input (->> "day2.txt"
-                   io/resource
-                   slurp
-                   clojure.string/trim
-                   parse-input
-                   (map #(Integer/parseInt %))
-                   vec
-                   prepare-program)]
+(def input (->> "day2.txt"
+            io/resource
+            slurp
+            clojure.string/trim
+            parse-input
+            (map #(Integer/parseInt %))
+            vec))
+
+
+(defn run [a b]
+  (let [in (prepare-program input a b)]
     (first
      (reduce (fn [ret [opcode a b position]]
                (case opcode
                  99 (reduced ret)
                  1 (assoc ret position (+ (get ret a) (get ret b)))
                  2 (assoc ret position (* (get ret a) (get ret b)))))
-             input
-             (partition 4 input)))))
+             in
+             (partition 4 in)))))
+
+(defn find-answer []
+  (for [a (range 100) b (range 100)
+        :let [x (run a b)]
+        :while (= 19690720 x)]
+    (+ b (* 100 a))))
 
 (comment
-  (run)
+  ;; (run 12 2)
+
+  (find-answer)
+
   )
